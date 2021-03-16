3fe2cc92dc8016150f974712afc1e970e6acf46f
Ian Mernagh <s014396@students.lmsd.org>
2021-03-16 08:44:38 -0400

    Initial Commit

---
+++ taskMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  taskMe
+//
+//  Created by Ian Mernagh (student LM) on 3/16/21.
+//  Copyright © 2021 Ian Mernagh (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
