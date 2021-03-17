bb326701369229d00d63ff014f826076ae852f5c
Ian Mernagh <s014396@students.lmsd.org>
2021-03-17 10:37:00 -0400

    firebaseauth

--- taskMe/ContentView.swift
+++ taskMe/ContentView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ContentView: View {
     var body: some View {
---
+++ taskMe/SignUpView.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpView.swift
+//  taskMe
+//
+//  Created by Ian Mernagh (student LM) on 3/17/21.
+//  Copyright © 2021 Ian Mernagh (student LM). All rights reserved.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
