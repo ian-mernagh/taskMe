d89ed3756293a8e31fc7c9a9a86e9abe79520eb1
Gal Yovel <s028548@students.lmsd.org>
2021-04-14 09:18:59 -0400

    I updated it to add a contact view style navigation

---
+++ taskMe/Views/WorkersList.swift
@@ -0,0 +1,51 @@
+//
+//  WorkersList.swift
+//  taskMe
+//
+//  Created by Gal Yovel (student LM) on 4/14/21.
+//  Copyright © 2021 Ian Mernagh (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct WorkersList: View {
+    
+    @Binding var workers : [Worker]
+    
+    var body: some View {
+        List(workers){ worker in
+            HStack{
+                
+                Image(worker.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .clipped()
+                
+                VStack(alignment: .leading){
+                    Text(worker.name).font(.system(size: 30))
+                        .foregroundColor(.orange)
+                    Text(worker.email)
+                    
+                }
+            }.background(Color.white.opacity(0.7))
+                .cornerRadius(20)
+                .padding(.horizontal, 10)
+        }
+    }
+}
+
+struct WorkersList_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkersList(workers: Binding.constant([Worker(image: "ben", name: "Ben", email: "BenSmith@NewWaveComputers.com"),
+                                                 Worker(image: "aslan", name: "Aslan", email: "aslan@NewWaveComputers.com"),
+                                                 Worker(image: "humphrey", name: "Humphrey", email: "humphrey@NewWaveComputers.com"),
+                                                 Worker(image: "joseph", name: "Joseph", email: "joseph@NewWaveComputers.com"),
+                                                 Worker(image: "kelly", name: "Kelly", email: "kelly@NewWaveComputers.com"),
+                                                 Worker(image: "michael", name: "Michael", email: "michael@NewWaveComputers.com"),
+                                                 Worker(image: "prince", name: "Prince", email: "prince@NewWaveComputers.com"),
+                                                 Worker(image: "tyler", name: "Tyler", email: "tyler@NewWaveComputers.com")
+            ].sorted {$0.name < $1.name}))
+    }
+}
+
